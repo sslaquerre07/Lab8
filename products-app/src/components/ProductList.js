26b752f443ff0e91f974685020fa4bd11d508bbe
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2024-03-13 15:12:05 -0600

    4.1.2 Done

--- products-app/src/components/ProductList.js
+++ products-app/src/components/ProductList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { fetchProducts } from '../services/apiService';
 import { useProductsContext } from '../context/ProductsContext';
+import DeleteButton from './DeleteButton';
 
 const ProductList = () => {
     const { products, setProducts } = useProductsContext();
@@ -13,10 +14,17 @@ const ProductList = () => {
         getProducts();
     }, [setProducts]);
 
+    const handleProductDeleted = (deletedProductId) =>{
+        setProducts(currentProducts => currentProducts.filter(product => product.id !== deletedProductId));
+    }
+
     return (
         <div>
             {products.map((product) => (
-                <div key={product.id}>{product.title}</div>
+                <div key={product.id}>
+                    {product.title}
+                    <DeleteButton productId={product.id} onProductDeleted={handleProductDeleted} />
+                </div>
             ))}
         </div>
     );
