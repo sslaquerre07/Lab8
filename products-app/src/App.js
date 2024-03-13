86393c5418ae1bce1e14997eb969106a6c6f2834
Robert Meyer <robert.meyer@ucalgary.ca>
2024-03-13 16:06:03 -0600

    Minor fixes

--- products-app/src/App.js
+++ products-app/src/App.js
@@ -1,40 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { ProductsProvider } from './context/ProductsContext'; // Adjust the import path as necessary
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
-import ProductForm from './components/ProductForm'
+import ProductForm from './components/ProductForm';
 import NotFoundPage from './components/NotFoundPage';
 import HomePage from './components/HomePage';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHome, faList, faPlus} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faList, faPlus } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { ProductsProvider } from './context/ProductsContext';
 
-function App(){
-  return(
-    <div className = "App">
+function App() {
+  return (
+    <div className="App">
       <ProductsProvider>
         <Router>
           <div>
-            {/*Navigation Links*/}
-            <nav className="navbar navbar-expand navbar-light fullwidth">
-              <div className='container-fluid'>
-                <div className="collapse nav-bar-collapse" id="navbarNav">
-                  <ul>
-                    <li className='nav-item'>
+            {/* Navigation Links */}
+            <nav className="navbar navbar-expand navbar-light full-width">
+              <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarNav">
+                  <ul className="navbar-nav nav-full-width">
+                    <li className="nav-item">
                       <Link className="nav-link" to="/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home">
                         <FontAwesomeIcon icon={faHome} />
                       </Link>
                     </li>
-                    <li className='nav-item'>
+                    <li className="nav-item">
                       <Link className="nav-link" to="/products" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Product List">
                         <FontAwesomeIcon icon={faList} />
                       </Link>
                     </li>
-                    <li className='nav-item'>
-                      <Link className="nav-link" to="/add" data-bs-toggle="tooltip" data-bs-placement="bottom" title="AddProduct">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/product/add" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Add Product">
                         <FontAwesomeIcon icon={faPlus} />
                       </Link>
                     </li>
@@ -42,19 +41,24 @@ function App(){
                 </div>
               </div>
             </nav>
-            {/*Routes for different pages */}
-            <Routes>
-              <Route path="/products" element={<ProductList />} />
-              <Route path="/" element={<HomePage />} />
-              <Route path="*" element={<NotFoundPage />} /> {/* Catch all
-              other route*/}
 
-            </Routes>
+
+            {/* Routes for different pages */}
+            <div className="container mt-3">
+              <Routes>
+                <Route path="/products" element={<ProductList />} />
+                <Route path="/product/:productId" element={<ProductDetails />} />
+                <Route path="/product/add" element={<ProductForm />} />
+                <Route path="/product/edit/:productId" element={<ProductForm />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+            </div>
           </div>
         </Router>
       </ProductsProvider>
     </div>
   );
-};
+}
 
 export default App;
