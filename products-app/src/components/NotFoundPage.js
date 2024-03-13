944c431fff8ce0ab6deaf7185922a33a1561e9a2
Robert Meyer <robert.meyer@ucalgary.ca>
2024-03-13 14:01:47 -0600

    Section 3.5.2 and 3.5.3 complete

---
+++ products-app/src/components/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h2>404 Not Found</h2>
+            <p>The page you're looking for does not exist.</p>
+        </div>
+    );
+};
+export default NotFoundPage;
